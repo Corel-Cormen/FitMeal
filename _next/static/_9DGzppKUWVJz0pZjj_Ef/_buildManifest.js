871c8c2bede5473b9ee83bcb4019fc1a63771957
CorelCormen <kornel1997k@gmail.com>
2026-04-24 21:09:31 +0200

    Updates

--- _next/static/_9DGzppKUWVJz0pZjj_Ef/_buildManifest.js
+++ _next/static/_9DGzppKUWVJz0pZjj_Ef/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/FitMeal//_next/:path+",
+        "destination": "/FitMeal/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
